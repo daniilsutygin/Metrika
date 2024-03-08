fb05f2632cd666850ff6dd2ab42f5381145bf309
Daniil <diamondlegos02@gmail.com>
2024-03-08 20:14:53 +0700

    Update index.html & style.css

---
+++ js/app.js
@@ -0,0 +1,52 @@
+// popup
+const POPUP_OPENED_CLASSNAME = 'popup_open';
+const BODY_FIXED_CLASSNAME = 'body_fixed';
+
+const bodyNode = document.querySelector('body');
+const popupNode = document.querySelector('#popup');
+const btnOpenNode = document.querySelector('#services__btn');
+const popupContentNode = document.querySelector('#popup__content')
+const btnCloseNode = document.querySelector('#popup__close-btn');
+
+btnOpenNode.addEventListener('click', togglePopup);
+btnCloseNode.addEventListener('click', togglePopup);
+
+popupNode.addEventListener('click', (event) => {
+    const isClickOutsideContent = !event.composedPath().includes(popupContentNode)
+
+    if (isClickOutsideContent) {
+        togglePopup();
+    }
+})
+
+function togglePopup() {
+    popupNode.classList.toggle(POPUP_OPENED_CLASSNAME);
+    bodyNode.classList.toggle(BODY_FIXED_CLASSNAME);
+}
+
+
+// burger menu
+
+const BURGER_OPENED_CLASSNAME = 'burger_open';
+const BURGER_BTN_OPENED_CLASSNAME = 'burger-btn_open';
+
+const burgerNode = document.querySelector('#burger');
+const burgerBtnNode = document.querySelector('#burger-btn');
+const burgerContentNode = document.querySelector('#burger__content')
+
+burgerBtnNode.addEventListener('click', toggleBurger);
+
+burgerNode.addEventListener('click', (event) => {
+    const isClickOutsideContent = !event.composedPath().includes(burgerContentNode)
+
+    if (isClickOutsideContent) {
+        toggleBurger();
+    }
+})
+
+function toggleBurger() {
+    burgerNode.classList.toggle(BURGER_OPENED_CLASSNAME);
+    bodyNode.classList.toggle(BODY_FIXED_CLASSNAME);
+    burgerBtnNode.classList.toggle(BURGER_BTN_OPENED_CLASSNAME);
+}
+
